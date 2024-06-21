a9f5b6936b675af11792ee621b0442584e916ff3
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 22:06:33 +0200

    refactor: redux saga config

--- src/store/sagas/config.ts
+++ src/store/sagas/config.ts
@@ -1,16 +1,18 @@
 import {all} from 'redux-saga/effects'; // Import 'all' effect from redux-saga to run multiple sagas concurrently
 
-import orderRootSaga from './order/saga';
+import {getFunctionTryCatchWrapped as tryCatch} from '../../utils';
 import orderListRootSaga from './orderList/saga'; // Import the root saga for order list functionality
+
 /**
  * The root saga that combines all other sagas in the application.
  * Uses the 'all' effect to run multiple sagas concurrently.
  *
  * This function should be imported and run in the Redux store configuration.
  */
-export default function* rootSaga() {
-  yield all([
-    orderListRootSaga(), // Combine the order list root saga
-    orderRootSaga(),
-  ]);
-}
+export default tryCatch(function getRootSaga() {
+  return function* rootSaga() {
+    yield all([
+      orderListRootSaga(), // Combine the order list root saga
+    ]);
+  };
+})();
