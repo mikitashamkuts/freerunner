1eb66747a70b5c25ee3438379f672238f58cdc59
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 22:05:13 +0200

    refactor: order list saga

--- src/store/sagas/orderList/saga.ts
+++ src/store/sagas/orderList/saga.ts
@@ -2,7 +2,9 @@ import {AxiosResponse} from 'axios';
 import {call, put, takeLatest} from 'redux-saga/effects';
 
 import {sendFetchOrderListRequest} from '../../../api';
+import {exceptionList, httpResponceStatusList} from '../../../constants';
 import {OrderType} from '../../../types';
+import {getExceptionCaptured, getFunctionTryCatchWrapped as tryCatch} from '../../../utils';
 import {
   fetchOrderListFailure,
   fetchOrderListRequest,
@@ -16,23 +18,29 @@ interface FetchOrdersResponse {
   };
 }
 
-// Saga to fetch orders
-function* fetchOrders() {
-  try {
-    const response: AxiosResponse<FetchOrdersResponse> = yield call(sendFetchOrderListRequest);
-    const orders = response.data.data.orders;
-    yield put(fetchOrderListSuccess(orders));
-  } catch (error) {
-    yield put(fetchOrderListFailure());
+// Saga to fetch order list
+const fetchOrderListSaga = tryCatch(function* fetchOrderListSagaSafe() {
+  const response: AxiosResponse<FetchOrdersResponse> = yield call(sendFetchOrderListRequest);
+  switch (response.status) {
+    case httpResponceStatusList.OK:
+      const orders = response.data.data.orders;
+      yield put(fetchOrderListSuccess(orders));
+      break;
+    default:
+      getExceptionCaptured(fetchOrderListSagaSafe, exceptionList.Network, response);
+      yield put(fetchOrderListFailure());
+      break;
   }
-}
+});
 
 // Watcher saga to watch for fetchOrderListRequest actions
-function* watchFetchOrders() {
-  yield takeLatest(fetchOrderListRequest.type, fetchOrders);
-}
+const watchFetchOrderListRequest = tryCatch(function* watchFetchOrderListRequestSafe() {
+  yield takeLatest(fetchOrderListRequest.type, fetchOrderListSaga);
+});
 
 // Root saga to export and run all watcher sagas
-export default function* rootSaga() {
-  yield watchFetchOrders();
-}
+export default tryCatch(function getOrderListRootSaga() {
+  return function* rootSaga() {
+    yield watchFetchOrderListRequest();
+  };
+})();
