ac29c80d65d8b9b7bd90ba756b201ea506c89229
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 19:12:26 +0200

    docs: get exeption captured function ts annotation

--- src/utils/getExceptionCaptured/function.ts
+++ src/utils/getExceptionCaptured/function.ts
@@ -1,10 +1,15 @@
 import * as Sentry from '@sentry/react-native';
 
 import {exceptionList, isDevelopmentEnvironment} from '../../constants';
-import {getFunctionTryCatchWrapped} from '../getFunctionTryCatchWrapped';
 
-const getExceptionCaptured = (context: {name: string}, exeption: keyof typeof exceptionList) => {
-  const exeptionData = {place: context?.name, exeption};
+/**
+ * Captures and logs exceptions based on the environment.
+ *
+ * @param {{name: string}} context - The context where the exception occurred.
+ * @param {keyof typeof exceptionList} exception - The exception to be captured.
+ */
+const getExceptionCaptured = (context: {name: string}, exception: keyof typeof exceptionList) => {
+  const exeptionData = {place: context?.name, exception};
   if (isDevelopmentEnvironment) {
     console.warn('Exception Captured: ', exeptionData);
   } else {
@@ -12,4 +17,5 @@ const getExceptionCaptured = (context: {name: string}, exeption: keyof typeof ex
   }
 };
 
-export default getFunctionTryCatchWrapped(getExceptionCaptured);
+// intentionaly unwrapped in tryCatch in case of occurance the unhadnled error to captured by the extenral handler
+export default getExceptionCaptured;
