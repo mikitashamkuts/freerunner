fe8efb80238b553aa844133f528fa9bd2c2badeb
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 22:04:27 +0200

    refactor: order list slice

--- src/store/slices/orderList/slice.ts
+++ src/store/slices/orderList/slice.ts
@@ -1,6 +1,8 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
 import {loadingStatusList} from '../../../constants';
 import {OrderType} from '../../../types';
+import {getFunctionTryCatchWrapped as tryCatch} from '../../../utils';
 
 // Define the shape of the state
 interface OrderListStateType {
@@ -15,46 +17,47 @@ const initialState: OrderListStateType = {
 };
 
 // Create a slice for orders with actions and reducers
-const ordersSlice = createSlice({
-  name: 'orderList',
-  initialState,
-  reducers: {
-    /**
-     * Sets the loading status when the fetch order list request is initiated.
-     * @param state - Current state of the order list.
-     */
-    fetchOrderListRequest(state) {
-      state.status = loadingStatusList.Loading;
+const orderListSlice = tryCatch(function getOrderListSlice() {
+  return createSlice({
+    name: 'orderList',
+    initialState,
+    reducers: {
+      /**
+       * Sets the loading status when the fetch order list request is initiated.
+       * @param state - Current state of the order list.
+       */
+      fetchOrderListRequest(state) {
+        tryCatch(function fetchOrderListRequestSafe() {
+          state.status = loadingStatusList.Loading;
+        })();
+      },
+      /**
+       * Updates the order list and sets the status to success.
+       * @param state - Current state of the order list.
+       * @param action - Action containing the payload with the list of orders.
+       */
+      fetchOrderListSuccess(state, action: PayloadAction<OrderType[]>) {
+        tryCatch(function fetchOrderListSuccessSafe() {
+          state.list = action.payload;
+          state.status = loadingStatusList.Success;
+        })();
+      },
+      /**
+       * Sets the error status when the fetch order list request fails.
+       * @param state - Current state of the order list.
+       */
+      fetchOrderListFailure(state) {
+        tryCatch(function fetchOrderListFailureSafe() {
+          state.status = loadingStatusList.Error;
+        })();
+      },
     },
-    /**
-     * Updates the order list and sets the status to success.
-     * @param state - Current state of the order list.
-     * @param action - Action containing the payload with the list of orders.
-     */
-    fetchOrderListSuccess(state, action: PayloadAction<OrderType[]>) {
-      state.list = action.payload;
-      state.status = loadingStatusList.Success;
-    },
-    /**
-     * Sets the error status when the fetch order list request fails.
-     * @param state - Current state of the order list.
-     */
-    fetchOrderListFailure(state) {
-      state.status = loadingStatusList.Error;
-    },
-    /**
-     * Sets the loading status when the fetch order list request is initiated.
-     * @param state - Current state of the order list.
-     */
-    refreshOrderListRequest(state) {
-      state.status = loadingStatusList.Loading;
-    },
-  },
-});
+  });
+})();
 
 // Export actions to be used in components and sagas
 export const {fetchOrderListRequest, fetchOrderListSuccess, fetchOrderListFailure} =
-  ordersSlice.actions;
+  orderListSlice.actions;
 
 // Export the reducer to be used in the store
-export default ordersSlice.reducer;
+export default orderListSlice.reducer;
