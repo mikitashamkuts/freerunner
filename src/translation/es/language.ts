62117d7d22058de0bac524613a648691959c645d
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 19:16:37 +0200

    remove: es language abandoned fileds

--- src/translation/es/language.ts
+++ src/translation/es/language.ts
@@ -11,34 +11,5 @@ export const es: TranslationType = {
         },
       },
     },
-    orderListItem: {
-      numberOfArticles: {
-        single: 'artículo',
-        multiple: 'artículos',
-      },
-      orderStatus: {
-        completed: 'Compra completada',
-        pending: 'Cobro de:',
-      },
-    },
-    orderDetailScreen: {
-      paymentStatus: 'Estado del pago',
-      referenceNumber: 'ref.',
-      numberOfShippedArticles: 'enviados',
-      orderStatus: {
-        pending: {
-          title: 'Cobro programado',
-          subtitle: 'Recibirás la compra pronto',
-        },
-        completed: {
-          title: 'Compra completada',
-          subtitle: 'Pagado el',
-        },
-      },
-      total: 'Total:',
-      payButton: {
-        title: 'Pagar ahora',
-      },
-    },
   },
 };
