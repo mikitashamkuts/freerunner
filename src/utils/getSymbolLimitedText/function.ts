a48ac49d10831ed41e6cd952de2564041dd4eb4b
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 19:08:35 +0200

    docs: get symbol limited text ts annotation

--- src/utils/getSymbolLimitedText/function.ts
+++ src/utils/getSymbolLimitedText/function.ts
@@ -1,8 +1,15 @@
 import {getFunctionTryCatchWrapped} from '../getFunctionTryCatchWrapped';
 
+/**
+ * Limits text to a specified length, appending ellipsis if truncated.
+ *
+ * @param {string} text - The text to be limited.
+ * @param {number} limit - The maximum length of the text.
+ * @returns {string} - The limited text with ellipsis if truncated, or an empty string if the input is invalid.
+ */
 function getSymbolLimitedText(text: string, limit: number) {
   if (!!text && typeof text === 'string') {
-    return text.length < limit ? `${text}` : `${text.substring(0, limit - 3)}...`;
+    return text.length <= limit ? text : `${text.substring(0, limit - 3)}...`;
   }
   return '';
 }
