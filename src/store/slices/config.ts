ad3bb68b33159d93f8ee40c35a947c8e49a7b274
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 22:07:21 +0200

    remove: abandoned redux codebase exports

--- src/store/slices/config.ts
+++ src/store/slices/config.ts
@@ -1,8 +1,6 @@
 // Define the structure of the global state
-import orderSliceReducer from './order/slice';
 import orderListReducer from './orderList/slice';
 
 export const stateStructure = {
   orderList: orderListReducer,
-  order: orderSliceReducer,
 };
