fd3fb4ee6dc2bb717d96804285a11f33764008c3
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-21 19:09:57 +0200

    refactor: get haptic feedback triggered function

--- src/utils/getHapticFeedbackTriggered/function.ts
+++ src/utils/getHapticFeedbackTriggered/function.ts
@@ -1,9 +1,22 @@
-import {trigger} from 'react-native-haptic-feedback';
+import {HapticFeedbackTypes, trigger} from 'react-native-haptic-feedback';
 
+import {exceptionList, hapticFeedbackModeList} from '../../constants';
+import {getExceptionCaptured} from '../getExceptionCaptured';
 import {getFunctionTryCatchWrapped} from '../getFunctionTryCatchWrapped';
 
-const getHapticFeedbackTriggered = () => {
-  trigger('soft');
-};
+/**
+ * Triggers haptic feedback with an optional mode.
+ *
+ * @param {keyof typeof HapticFeedbackTypes} [hapticFeedbackMode] - The haptic feedback mode to trigger. Defaults to a specified default mode if not provided.
+ */
+function getHapticFeedbackTriggered(hapticFeedbackMode?: keyof typeof HapticFeedbackTypes) {
+  if (trigger && hapticFeedbackMode) {
+    trigger(hapticFeedbackMode || hapticFeedbackModeList.Default);
+  } else {
+    hapticFeedbackMode
+      ? getExceptionCaptured(getHapticFeedbackTriggered, exceptionList.UnavailableModule)
+      : getExceptionCaptured(getHapticFeedbackTriggered, exceptionList.InvalidParam);
+  }
+}
 
 export default getFunctionTryCatchWrapped(getHapticFeedbackTriggered);
