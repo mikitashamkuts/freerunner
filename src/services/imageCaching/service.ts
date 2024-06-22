26cbe8a149f38da710004c5c774a6ac6ecfc0e5a
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-22 17:54:30 +0200

    refactor: image caching services try catch wrapped

--- src/services/imageCaching/service.ts
+++ src/services/imageCaching/service.ts
@@ -1,19 +1,29 @@
 import {CacheManager} from '@georstat/react-native-image-cache';
 import {Dirs, FileSystem} from 'react-native-file-access';
-import {defaultAnimationDuration, imageCacheSizeLimit, isAndroid} from '../../constants';
 
-export const initImageCaching = async (
-  setIsImageCachaingEnabled: (isImageCachaingEnabled: boolean) => void,
-) => {
+import {defaultAnimationDuration, imageCacheSizeLimit, isAndroid} from '../../constants';
+import {getFunctionTryCatchWrapped as tryCatch} from '../../utils';
+
+/**
+ * Initializes the image caching by configuring the CacheManager.
+ * It sets up the cache directory based on the platform (Android or iOS),
+ * creates the directory if it doesn't exist, and enables image caching.
+ *
+ * @param {function} setIsImageCachingEnabled - Callback function to set the image caching enabled state.
+ */
+export const initImageCaching = tryCatch(async function initImageCachingSafe(
+  setIsImageCachingEnabled: (isImageCachingEnabled: boolean) => void,
+) {
   const {DatabaseDir, LibraryDir} = Dirs;
   const baseDir = `${isAndroid ? DatabaseDir : LibraryDir}/images_cache/`;
 
+  // Check if the directory exists, if not, create it
   const dirExists = await FileSystem.exists(baseDir);
   if (!dirExists) {
     await FileSystem.mkdir(baseDir);
   }
 
-  // Configure the CacheManager
+  // Configure the CacheManager with the specified directory and settings
   CacheManager.config = {
     baseDir: baseDir,
     blurRadius: 15,
@@ -21,15 +31,26 @@ export const initImageCaching = async (
     sourceAnimationDuration: defaultAnimationDuration,
     thumbnailAnimationDuration: defaultAnimationDuration,
   };
-  setIsImageCachaingEnabled(true);
-};
 
-export const manageCacheCapacity = async (
+  setIsImageCachingEnabled(true);
+});
+
+/**
+ * Manages the cache capacity by checking the current cache size.
+ * If the cache size exceeds the limit, it clears the cache.
+ *
+ * @param {function} setIsImageCachingCapacityManaged - Callback function to set the image caching capacity managed state.
+ */
+export const manageCacheCapacity = tryCatch(async function manageCacheCapacitySafe(
   setIsImageCachingCapacityManaged: (setIsImageCachingCapacityManaged: boolean) => void,
-) => {
+) {
+  // Get the current cache size
   const cacheSize = await CacheManager.getCacheSize();
+
+  // If the cache size exceeds the limit, clear the cache
   if (cacheSize >= imageCacheSizeLimit) {
     await CacheManager.clearCache();
   }
+
   setIsImageCachingCapacityManaged(true);
-};
+});
