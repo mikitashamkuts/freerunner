8249bdbe7462cc80c599834f97a7be4687c36320
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-23 16:03:34 +0200

    docs: use theme hook annotation & comments

--- src/hooks/useTheme/hook.ts
+++ src/hooks/useTheme/hook.ts
@@ -1,4 +1,3 @@
-// useTheme.ts
 import {useMemo} from 'react';
 import {StyleSheet, useColorScheme} from 'react-native';
 
@@ -9,6 +8,11 @@ type StyleSheetStyleObjectType = {
   [key: string]: DefaultStyleSheetStyleType;
 };
 
+/**
+ * Custom hook to apply theme-based styles.
+ * @param styles - An object containing base styles and themed styles.
+ * @returns An object containing merged styles based on the current theme.
+ */
 function useTheme(styles: StyleSheetStyleObjectType): StyleSheetStyleObjectType {
   const theme = useColorScheme();
   const defaultTheme = 'light';
@@ -22,13 +26,14 @@ function useTheme(styles: StyleSheetStyleObjectType): StyleSheetStyleObjectType 
     const mergedStyleObject = {} as any;
 
     styleList.forEach((styleName: string) => {
-      const baseStyle = styles[styleName];
-      const themedStyle = styles[`${theme}_${styleName}`];
+      const baseStyle = styles[styleName]; // Get the base style
+      const themedStyle = styles[`${theme}_${styleName}`]; // Get the themed style for the current theme
+      // Merge the base style and themed style
       mergedStyleObject[styleName] = StyleSheet.compose(baseStyle, themedStyle);
     });
 
     return StyleSheet.create(mergedStyleObject);
-  }, [theme, styles]);
+  }, [theme, styles]); // Recompute styles only when theme or styles change
 }
 
 export default getFunctionTryCatchWrapped(useTheme);
