6a3fb20da99223c9058c7038959d2c926209c4a6
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-23 14:29:53 +0200

    test: status bar snapshot

--- src/components/StatusBar/test.tsx
+++ src/components/StatusBar/test.tsx
@@ -7,12 +7,22 @@ import 'react-native';
 import renderer from 'react-test-renderer';
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
-import {default as StatusBar} from './component';
 
-it('Renders correctly: ', () => {
-  renderer.create(<StatusBar />);
-});
+import StatusBar from './component';
 
-it('Matches previous snapshot: ', () => {
-  expect(renderer.create(<StatusBar />).toJSON()).toMatchSnapshot();
+describe('StatusBar Component', () => {
+  it('Renders with default props and matches previous snapshot: ', () => {
+    const tree = renderer.create(<StatusBar />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders with light style and matches previous snapshot: ', () => {
+    const tree = renderer.create(<StatusBar style="light" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders with dark style and matches previous snapshot: ', () => {
+    const tree = renderer.create(<StatusBar style="dark" />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
