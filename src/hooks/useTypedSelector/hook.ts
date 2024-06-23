67facce5e8cd8c8078ca31e4e4672e96eae00f7d
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-23 16:04:27 +0200

    docs: use typed selector hook comments

--- src/hooks/useTypedSelector/hook.ts
+++ src/hooks/useTypedSelector/hook.ts
@@ -2,7 +2,7 @@ import {useSelector} from 'react-redux';
 
 import {GlobalStateType} from '../../types';
 
-// Use throughout your app instead of plain `useSelector`
+// Use throughout the app instead of plain `useSelector`
 const useTypedSelector = useSelector.withTypes<GlobalStateType>();
 
 export default useTypedSelector;
