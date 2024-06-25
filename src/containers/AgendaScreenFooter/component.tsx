455f08842f6ff7de07a3fc97606295a24853dba0
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-25 18:08:04 +0200

    feat: agenda screen footer container with agenda slot list filter

--- src/containers/AgendaScreenFooter/component.tsx
+++ src/containers/AgendaScreenFooter/component.tsx
@@ -1,17 +1,38 @@
-import React, {FC, memo} from 'react';
+import React, {FC, memo, useCallback, useState} from 'react';
+import {useTranslation} from 'react-i18next';
 import {Pressable, StyleSheet, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import {whyDidItRenderConfig} from '../../../debug';
 import {Text} from '../../components';
 import {useTheme} from '../../hooks';
+import {getFunctionTryCatchWrapped as tryCatch} from '../../utils';
 
 import {Props, styles} from '.';
 
-const AgendaScreenFooter: FC<Props> = ({}) => {
+const filterOptionSequence = ['all', 'booked', 'available'];
+
+const AgendaScreenFooter: FC<Props> = ({config}) => {
+  const [filterOptionIndex, setFilterOptionIndex] = useState(0);
   const insets = useSafeAreaInsets();
+  const {t} = useTranslation();
 
   const themedStyles = useTheme(styles);
+
+  const {setAgendaSlotListFilter} = config;
+
+  const handleOnFilterOptionPress = useCallback(() => {
+    tryCatch(function getFilterOptionCalculated() {
+      if (filterOptionIndex >= filterOptionSequence.length - 1) {
+        setAgendaSlotListFilter(filterOptionSequence[0]);
+        setFilterOptionIndex(0);
+      } else {
+        const shift = filterOptionIndex + 1;
+        setAgendaSlotListFilter(filterOptionSequence[shift]);
+        setFilterOptionIndex(shift);
+      }
+    })();
+  }, [filterOptionIndex, setAgendaSlotListFilter]);
 
   const dynamicStyles = StyleSheet.create({
     container: {
@@ -21,11 +42,25 @@ const AgendaScreenFooter: FC<Props> = ({}) => {
 
   return (
     <View style={[themedStyles.container, dynamicStyles.container]}>
-      <Pressable style={themedStyles.buttonContainer}>
-        <Text color="Action" text="Today" />
+      <Pressable
+        accessibilityLabel={t('agendaScreen.footer.todayButton.accessibilityLabel')}
+        accessibilityHint={t('agendaScreen.footer.todayButton.accessibilityHint')}
+        style={themedStyles.buttonContainer}>
+        <Text color="Action" text={t('agendaScreen.footer.todayButton.text')} />
       </Pressable>
-      <Pressable style={themedStyles.buttonContainer}>
-        <Text color="Action" text="All" />
+      <Pressable
+        accessibilityLabel={t(
+          `agendaScreen.footer.filterButton.${filterOptionSequence[filterOptionIndex]}.accessibilityLabel`,
+        )}
+        accessibilityHint={`agendaScreen.footer.filterButton.${filterOptionSequence[filterOptionIndex]}.accessibilityHint`}
+        style={themedStyles.buttonContainer}
+        onPress={handleOnFilterOptionPress}>
+        <Text
+          color="Action"
+          text={t(
+            `agendaScreen.footer.filterButton.${filterOptionSequence[filterOptionIndex]}.text`,
+          )}
+        />
       </Pressable>
     </View>
   );
