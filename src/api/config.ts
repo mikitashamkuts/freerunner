385be08021f7e214fb49703ecdca42333f81196e
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 00:08:27 +0200

    feat: api origin config

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,13 +1,10 @@
-// api/axiosConfig.ts
-
 import axios from 'axios';
 
-import {DEVELOPMENT_ORIGIN, PRODUCTION_ORIGIN} from '@env'; // Import the Sentry DSN (Data Source Name) from environment variables
+import {DEVELOPMENT_ORIGIN, PRODUCTION_ORIGIN} from '@env';
 import {isDevelopmentEnvironment} from '../constants';
 
-// Set the base URL based on the environment
 const baseURL = isDevelopmentEnvironment ? DEVELOPMENT_ORIGIN : PRODUCTION_ORIGIN;
 
 export const axiosInstance = axios.create({
-  baseURL: `${baseURL}/graphql`, // Common base URL for GraphQL
+  baseURL: `${baseURL}/api`,
 });
