158cc49df9bc30f348430f78c2d0ac9e75d22e17
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 00:16:10 +0200

    feat: container exports

---
+++ src/containers/index.ts
@@ -0,0 +1,6 @@
+export {AgendaScreenAgendaList} from './AgendaScreenAgendaList';
+export {AgendaScreenAgendaListItem} from './AgendaScreenAgendaListItem';
+export {AgendaScreenFooter} from './AgendaScreenFooter';
+export {AgendaScreenHeader} from './AgendaScreenHeader';
+export {AgendaScreenHeaderDateText} from './AgendaScreenHeaderDateText';
+export {AgendaScreenSelectedSlotBottomSheet} from './AgendaScreenSelectedSlotBottomSheet';
