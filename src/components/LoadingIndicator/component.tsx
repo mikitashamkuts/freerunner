982834bf975bcfaef300c9ab4391c500ce3b2f3a
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 00:07:42 +0200

    feat: loading indicator component

---
+++ src/components/LoadingIndicator/component.tsx
@@ -0,0 +1,20 @@
+import React, {FC, memo} from 'react';
+import {ActivityIndicator, View} from 'react-native';
+
+import {whyDidItRenderConfig} from '../../../debug';
+
+import {iconColorList} from '../../design';
+
+import {Props, styles} from '.';
+
+const LoadingIndicator: FC<Props> = ({}) => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} color={iconColorList.Action} />
+    </View>
+  );
+};
+
+LoadingIndicator.whyDidYouRender = whyDidItRenderConfig.UIComponentDebugActive;
+
+export default memo(LoadingIndicator);
---
+++ src/components/LoadingIndicator/index.ts
@@ -0,0 +1,3 @@
+export {default as LoadingIndicator} from './component';
+export * from './types';
+export type {Props as LoadingIndicatorInterface} from './types';
