19833f01558e021d5f3bdde80af0c1d76f259334
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 19:12:58 +0200

    refactor: day letter text component with try catch

--- src/components/DayLetterText/component.tsx
+++ src/components/DayLetterText/component.tsx
@@ -1,19 +1,27 @@
-import React, {FC, memo} from 'react';
+import React, {FC, memo, useMemo} from 'react';
 import {View, useColorScheme} from 'react-native';
 
 import {whyDidItRenderConfig} from '../../../debug';
 import {Text} from '../../components';
+import {getFunctionTryCatchWrapped as tryCatch} from '../../utils';
 
 import {Props} from '.';
 
 const DayLetterText: FC<Props> = ({text, containerStyle, ...props}) => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const color = useMemo(() => {
+    return tryCatch(function getDayLetterTextColor() {
+      return isDarkMode ? 'Light' : 'Dark';
+    })();
+  }, [isDarkMode]);
+
   return (
     <View style={containerStyle}>
       <Text
         fontWeight="SemiBold"
         fontSize="Small"
-        color={isDarkMode ? 'Light' : 'Dark'}
+        color={color}
         text={text}
         numberOfLines={1}
         {...props}
