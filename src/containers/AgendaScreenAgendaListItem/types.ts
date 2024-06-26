cb44d7c01e80b55e92438b9b5a7099f54123d19e
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 22:16:23 +0200

    type: agenda screen agenda list item on press prop

--- src/containers/AgendaScreenAgendaListItem/types.ts
+++ src/containers/AgendaScreenAgendaListItem/types.ts
@@ -1,3 +1,5 @@
 import {AgendaSlotType, CustomComponentProps} from '../../types';
 
-export interface Props extends CustomComponentProps, AgendaSlotType {}
+export interface Props extends CustomComponentProps, AgendaSlotType {
+  onPress: (value: AgendaSlotType) => void;
+}
