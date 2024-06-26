883cfad3ff4f7c5dbd1e7b3f25e057d3042d3db8
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 19:15:23 +0200

    refactor: agenda screen agenda list item with try catch

--- src/containers/AgendaScreenAgendaListItem/component.tsx
+++ src/containers/AgendaScreenAgendaListItem/component.tsx
@@ -1,29 +1,30 @@
-import React, {FC, memo} from 'react';
-import {Pressable, View, useColorScheme} from 'react-native';
+import React, {FC, memo, useCallback} from 'react';
+import {Pressable, View} from 'react-native';
 
 import {whyDidItRenderConfig} from '../../../debug';
 import {AgendaRowTimeText, Icon} from '../../components';
 import {hapticFeedbackModeList} from '../../constants';
 import {useTheme} from '../../hooks';
-import {getHapticFeedbackTriggered} from '../../utils';
+import {getHapticFeedbackTriggered, getFunctionTryCatchWrapped as tryCatch} from '../../utils';
 
 import {Props, styles} from '.';
 
 const AgendaScreenAgendaListItem: FC<Props> = item => {
-  const {Taken, Start, onPress} = item;
-  const isDarkMode = useColorScheme() === 'dark';
-
   const themedStyles = useTheme(styles);
 
+  const {Taken, Start, onPress} = item;
+
+  const handleOnPress = useCallback(() => {
+    tryCatch(function handleOnPressSafe() {
+      if (!Taken) {
+        onPress?.(item);
+        getHapticFeedbackTriggered(hapticFeedbackModeList.Default);
+      }
+    })();
+  }, [Taken, item, onPress]);
+
   return (
-    <Pressable
-      onPress={() => {
-        if (!Taken) {
-          onPress?.(item);
-          getHapticFeedbackTriggered(hapticFeedbackModeList.Default);
-        }
-      }}
-      style={themedStyles.container}>
+    <Pressable onPress={handleOnPress} style={themedStyles.container}>
       <View style={themedStyles.contentContainer}>
         <AgendaRowTimeText text={Start.slice(11).slice(0, 5)} />
         <Icon color={Taken ? 'Faded' : 'Action'} name={Taken ? 'CalendarChecked' : 'Plus'} />
