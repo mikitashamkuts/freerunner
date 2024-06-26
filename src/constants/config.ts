0104685a9102b5b373f0d2d20ecc96a9a0427062
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 11:41:34 +0200

    remove: constants abandoned values

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -17,18 +17,11 @@ export enum loadingStatusList {
 
 export const isDevelopmentEnvironment = process.env.NODE_ENV === 'development';
 
-export const defaultCurrency = {
-  symbol: '€',
-};
-
 export const imageCacheSizeLimit = 1815; // 1.8mb, aproximate equivalent of 50 cached logo icons
 
 export const defaultAnimationDuration = 375; // ms
 
 export const isAndroid = Platform.OS === 'android';
-
-export const priceDigitLimit = 15; // for preventing text overflow, presuming EUR currency is the only option
-export const overflowSymbolLimit = 16; // for preventing text overflow,
 
 export enum exceptionList {
   InvalidParam = 'InvalidParam', //'Undefined or Falsy or Incorrect Parameter',
