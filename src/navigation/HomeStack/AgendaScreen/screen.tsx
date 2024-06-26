e33d5f5f01064a09b07f6fba6bb5d2b628c2d380
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-06-26 19:17:22 +0200

    refactor: agenda screen with try catch

--- src/navigation/HomeStack/AgendaScreen/screen.tsx
+++ src/navigation/HomeStack/AgendaScreen/screen.tsx
@@ -1,4 +1,4 @@
-import {FC, useEffect, useMemo, useState} from 'react';
+import {FC, useCallback, useEffect, useMemo, useState} from 'react';
 
 import {View} from 'react-native';
 import {whyDidItRenderConfig} from '../../../../debug';
@@ -12,9 +12,29 @@ import {
 import {useTypedDispatch, useTypedSelector} from '../../../hooks';
 import {addSlotToBookedAgendaSlotList, fetchAgendaSlotListRequest} from '../../../store';
 import {AgendaSlotListType, AgendaSlotType} from '../../../types';
+
+import {
+  getAgendaSlotListWithLocalBooked,
+  getSlotsByDay,
+  getUniqueDays,
+  getWeekRangeText,
+  getFunctionTryCatchWrapped as tryCatch,
+} from '../../../utils';
 import {styles} from './styles';
 
 export type AgendaSlotListFilerType = 'all' | 'booked' | 'available';
+
+export type AgendaScreenConfigType = {
+  agendaSlotList: AgendaSlotListType;
+  setAgendaSlotListFilter: (value: AgendaSlotListFilerType) => void;
+  setSelectedAgendaSlot: (value: AgendaSlotType) => void;
+  setIsBottomSheetShown: (value: boolean) => void;
+  agendaSlot: AgendaSlotType;
+  selectedWeekShift: number;
+  previousToSelectedWeekDateText: string;
+  nextToSelectedWeekDateText: string;
+  setSelectedWeekShift: (value: number) => void;
+};
 
 const AgendaScreen: FC = () => {
   const [agendaSlotListFilter, setAgendaSlotListFilter] = useState<AgendaSlotListFilerType>('all');
@@ -28,104 +48,69 @@ const AgendaScreen: FC = () => {
 
   const dispatch = useTypedDispatch();
 
-  const handleAddSlotToBookedAgendaSlotList = slot => {
-    dispatch(addSlotToBookedAgendaSlotList(slot));
-  };
-  const getWeekRange = (weekShift: number = 0): string => {
-    const currentDate = new Date();
-    const dayOfWeek = currentDate.getDay();
-    const daysToMonday = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
-
-    const firstDayOfWeek = new Date(
-      currentDate.setDate(currentDate.getDate() + daysToMonday + weekShift * 7),
-    );
-    const lastDayOfWeek = new Date(currentDate.setDate(firstDayOfWeek.getDate() + 6));
-    const options = {day: 'numeric', month: 'long'} as const;
-    const firstDayString = firstDayOfWeek.toLocaleDateString('en-US', options);
-    const lastDayString = lastDayOfWeek.toLocaleDateString('en-US', options);
-    return `${firstDayString} - ${lastDayString}`;
-  };
-
-  function getSlotsByDay(slots, targetDate) {
-    // Format the target date to ensure consistency in comparison
-    const formattedTargetDate = new Date(targetDate).toISOString().split('T')[0];
-    // Filter slots to match only the specified target date
-    return slots.filter(slot => {
-      const slotDate = slot.Start.split('T')[0];
-      return slotDate === formattedTargetDate;
-    });
-  }
-
-  const getUniqueDays = (list: AgendaSlotListType): number => {
-    const uniqueDays = new Set(list.map(slot => slot.Start.split('T')[0]));
-    return Array.from(uniqueDays);
-  };
+  const handleAddSlotToBookedAgendaSlotList = useCallback(
+    slot => {
+      tryCatch(function handleAddSlotToBookedAgendaSlotListSafe() {
+        dispatch(addSlotToBookedAgendaSlotList(slot));
+      })();
+    },
+    [dispatch],
+  );
 
   const uniqueQayList = getUniqueDays(list);
 
-  const getAgendaSlotListWithLocalBooked = (list, bookedList) => {
-    // Create a map using stringified Start and End times as keys
-    const bookedListMap = new Map(bookedList.map(item => [`${item.Start}-${item.End}`, item]));
-
-    return list.map(slot => {
-      const {Start, End, Taken} = slot;
-      // Check if the current slot is in the bookedListMap
-      const isBooked = bookedListMap.has(`${Start}-${End}`);
+  const config: AgendaScreenConfigType = useMemo(() => {
+    return tryCatch(function getAgendaScreenConfig() {
       return {
-        Start,
-        End,
-        Taken: Taken || isBooked,
+        agendaSlotList: getAgendaSlotListWithLocalBooked(
+          getSlotsByDay(filteredAgendaSlotList, uniqueQayList[selectedDay]),
+          bookedList,
+        ),
+        setAgendaSlotListFilter,
+        setSelectedAgendaSlot,
+        setIsBottomSheetShown,
+        agendaSlot: selectedAgendaSlot,
+        selectedWeekShift,
+        previousToSelectedWeekDateText: getWeekRangeText(selectedWeekShift - 1),
+        nextToSelectedWeekDateText: getWeekRangeText(selectedWeekShift + 1),
+        setSelectedWeekShift,
+        uniqueDayList: uniqueQayList,
+        uniqueSelectedDay: uniqueQayList[selectedDay],
+        isPreviousToSelectedWeekDateAvailable: true,
+        setSelectedDay,
+        handleAddSlotToBookedAgendaSlotList,
       };
-    });
-  };
-
-  const config = useMemo(
-    () => ({
-      agendaSlotList: getAgendaSlotListWithLocalBooked(
-        getSlotsByDay(filteredAgendaSlotList, uniqueQayList[selectedDay]),
-        bookedList,
-      ), //filteredAgendaSlotList
-      setAgendaSlotListFilter,
-      setSelectedAgendaSlot,
-      setIsBottomSheetShown,
-      agendaSlot: selectedAgendaSlot,
-      selectedWeekShift,
-      previousToSelectedWeekDateText: getWeekRange(selectedWeekShift - 1),
-      nextToSelectedWeekDateText: getWeekRange(selectedWeekShift + 1),
-      setSelectedWeekShift,
-      uniqueDayList: uniqueQayList,
-      uniqueSelectedDay: uniqueQayList[selectedDay],
-      isPreviousToSelectedWeekDateAvailable: true,
-      setSelectedDay,
-      handleAddSlotToBookedAgendaSlotList,
-    }),
-    [
-      bookedList,
-      filteredAgendaSlotList,
-      handleAddSlotToBookedAgendaSlotList,
-      selectedAgendaSlot,
-      selectedDay,
-      selectedWeekShift,
-      uniqueQayList,
-    ],
-  );
+    })();
+  }, [
+    bookedList,
+    filteredAgendaSlotList,
+    handleAddSlotToBookedAgendaSlotList,
+    selectedAgendaSlot,
+    selectedDay,
+    selectedWeekShift,
+    uniqueQayList,
+  ]);
 
   useEffect(() => {
-    dispatch(fetchAgendaSlotListRequest(selectedWeekShift));
+    tryCatch(function getInitialDataLoaded() {
+      dispatch(fetchAgendaSlotListRequest(selectedWeekShift));
+    })();
   }, [dispatch, selectedWeekShift]);
 
   useEffect(() => {
-    if (agendaSlotListFilter === 'all') {
-      setFilteredAgendaSlotList(list);
-    }
-    if (agendaSlotListFilter === 'available') {
-      const availableAgendaSlotList = list.filter(({Taken}) => !Taken);
-      setFilteredAgendaSlotList(availableAgendaSlotList);
-    }
-    if (agendaSlotListFilter === 'booked') {
-      const bookedAgendaSlotList = list.filter(({Taken}) => Taken);
-      setFilteredAgendaSlotList(bookedAgendaSlotList);
-    }
+    tryCatch(function getAgendaSlotListFiltered() {
+      if (agendaSlotListFilter === 'all') {
+        setFilteredAgendaSlotList(list);
+      }
+      if (agendaSlotListFilter === 'available') {
+        const availableAgendaSlotList = list.filter(({Taken}) => !Taken);
+        setFilteredAgendaSlotList(availableAgendaSlotList);
+      }
+      if (agendaSlotListFilter === 'booked') {
+        const bookedAgendaSlotList = list.filter(({Taken}) => Taken);
+        setFilteredAgendaSlotList(bookedAgendaSlotList);
+      }
+    })();
   }, [agendaSlotListFilter, list]);
 
   return (
