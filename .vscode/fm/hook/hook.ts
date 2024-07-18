681006db847c620b6b543d4ed5f38965466daf3a
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-07-18 18:10:37 +0200

    feat: fm hook template

---
+++ .vscode/fm/hook/hook.ts
@@ -0,0 +1,12 @@
+import {useEffect, useState} from 'react';
+
+import {getFunctionTryCatchWrapped} from '@src/utils';
+
+/**
+ *  __name__ hook description.
+ * @param {paramName} param description
+ * @returns {returnName} return description
+ */
+function __name__() {}
+
+export default getFunctionTryCatchWrapped(__name__);
