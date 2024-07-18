20e7f6d9da5b39ce91c35449d92117bdff0890f2
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-07-18 14:42:39 +0200

    fix: alias config

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,14 @@ module.exports = {
   // Specifies the Babel plugins to be used. Plugins are individual transformations applied to the code.
   plugins: [
     [
+      'module-resolver',
+      {
+        alias: {
+          '@src': './src',
+        },
+      },
+    ],
+    [
       // The 'react-native-dotenv' plugin is used to load environment variables from a .env file.
       'module:react-native-dotenv',
       {
@@ -18,26 +26,5 @@ module.exports = {
     ],
     // The 'react-native-reanimated/plugin' plugin is required for React Native Reanimated library.
     ['react-native-reanimated/plugin'],
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@api': 'src/api/index',
-          '@components': './src/components/index',
-          '@constants': 'src/constants/index',
-          '@containers': 'src/containers/index',
-          '@design': 'src/design/index',
-          '@hooks': 'src/hooks/index',
-          '@navigation': 'src/navigation/index',
-          '@services': 'src/services/index',
-          '@store': 'src/store/index',
-          '@translation': 'src/translation/index',
-          '@types': 'src/types/index',
-          '@utils': 'src/utils/index',
-        },
-      },
-    ],
   ],
 };
