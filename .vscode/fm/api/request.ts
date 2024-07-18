641fefa3cadb0ee99b371e4e8c2c440d9595ca92
Mikita Shamkuts <mikitashamkuts@gmail.com>
2024-07-18 18:07:40 +0200

    feat: fm api template

---
+++ .vscode/fm/api/request.ts
@@ -0,0 +1,16 @@
+import {AxiosResponse} from 'axios';
+
+import {httpResponceStatusList} from '@src/constants';
+import axiosInstance from '../config';
+
+/**
+ * __name__ description
+ * @param {paramName} param description
+ * @returns {returnName} return description
+ */
+export const __name__ = async () => {
+  try {
+  } catch {}
+};
+
+export default __name__;
